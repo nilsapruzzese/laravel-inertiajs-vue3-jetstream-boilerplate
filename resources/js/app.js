31675613cb6c6a5c17647f49deae103ac353602c
Nils Vennemann <contact.nilsvennemann@gmail.com>
2022-03-13 22:00:41 +0100

    feat: first commit

---
+++ resources/js/app.js
@@ -0,0 +1,29 @@
+require('./bootstrap');
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
+
+// Plugins
+import { i18nVue } from "laravel-vue-i18n";
+
+// Mixins
+import * as base from './base';
+
+// Misc
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => require(`./Pages/${name}.vue`),
+    setup({ el, app, props, plugin }) {
+        return createApp({ render: () => h(app, props) })
+            .use(plugin)
+            .use(i18nVue)
+            .mixin({ methods: { route } })
+            .mixin(base)
+            .mount(el);
+    },
+});
+
+InertiaProgress.init({ color: '#4B5563' });
